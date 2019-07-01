3d6d9a68ad48902dd0ccaa728b2342dd157d7c89
lucasaf <ferreira.lucasalves@gmail.com>
2019-07-01 15:44:58 -0300

    creating login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { ReactiveFormsModule } from "@angular/forms";
 import { UserService } from './shared/user.service';
+import { LoginComponent } from './user/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
